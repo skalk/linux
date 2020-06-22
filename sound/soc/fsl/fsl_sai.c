8a740f87641e894868b8ed27eae90f4c29894b4b
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2020-06-22 16:28:12 +0200

    mnt reform2: audio-sai-workaround-rate-matching.patch from MNT Research GmbH

--- sound/soc/fsl/fsl_sai.c
+++ sound/soc/fsl/fsl_sai.c
@@ -348,21 +348,12 @@ static int fsl_sai_set_bclk(struct snd_soc_dai *dai, bool tx, u32 freq)
 
 		ret = clk_rate - ratio * freq;
 
-		/*
-		 * Drop the source that can not be
-		 * divided into the required rate.
-		 */
-		if (ret != 0 && clk_rate / ret < 1000)
-			continue;
-
 		dev_dbg(dai->dev,
 			"ratio %d for freq %dHz based on clock %ldHz\n",
 			ratio, freq, clk_rate);
 
 		if (ratio % 2 == 0 && ratio >= 2 && ratio <= 512)
 			ratio /= 2;
-		else
-			continue;
 
 		if (ret < savesub) {
 			savediv = ratio;
